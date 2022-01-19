d18a78bcf995c8b077390b1a55a7f0b56dbbe08c
Shezitt <shamirteranmustafa@gmail.com>
2022-01-19 14:11:40 -0400

    CodeChef Starters 22

---
+++ CodeChef/Starters 22/C.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef long double ld;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+#define lcm(a,b) a*b/__gcd(a,b)
+#define INF 1000000000
+#define pb push_back
+vector<int> ans;
+#define mp make_pair
+#define fast ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define dbg(x) cerr << #x << ": " << x  << endl;
+#define raya cerr << "=================================" << endl;
+#define FOR(i, x, y) for(int i=x; i<=y; ++i)
+const double PI = 3.141592653589793;
+const int MOD = 1e9+7;
+
+void solve(){
+	int n;
+	cin >> n;
+	if(n==2){
+		cout << "NO\n";
+		return;
+	}
+	cout << "YES\n";
+	int half = n/2;
+	if(n%2){
+		FOR(i, 1, half){
+			cout << i << ' ';
+		}
+		cout << n << ' ';
+		for(int i=n-1; i>half; --i){	
+			cout << i << ' ';
+		}
+	} else {
+		cout << half << ' ';
+		FOR(i, 1, half-1){
+			cout << i << ' ';
+		}
+		for(int i=n; i>half; --i){
+			cout << i << ' ';
+		}
+	}
+	cout << '\n';
+}
+
+int main(){
+   //fast;
+    int T = 1;
+    cin >> T;
+    while(T--)
+        solve();
+}
+
