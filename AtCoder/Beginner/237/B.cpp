4e76f191669052cc915e802630e1d5f55a944490
Shezitt <shamirteranmustafa@gmail.com>
2022-01-30 12:55:40 -0400

    AtCoder Beginner 237

---
+++ AtCoder/Beginner/237/B.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef long double ld;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+#define lcm(a,b) a*b/__gcd(a,b)
+#define INF 1000000000
+#define pb push_back
+#define mp make_pair
+#define fast ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define dbg(x) cerr << #x << ": " << x  << endl;
+#define raya cerr << "=================================" << endl;
+#define FOR(i, x, y) for(int i=x; i<=y; ++i)
+const double PI = 3.141592653589793;
+const int MOD = 1e9+7;
+
+void solve(){
+	int h, w;
+	cin >> h >> w;
+	vector<vector<int>> matrix(h, vector<int>(w));
+	FOR(i, 0, h-1){
+		FOR(j, 0, w-1){
+			cin >> matrix[i][j];
+		}
+	}
+	FOR(i, 0, w-1){
+		FOR(j, 0, h-1){
+			cout << matrix[j][i] << ' ';
+		}
+		cout << '\n';
+	}
+}
+
+int main(){
+    int T = 1;
+    //cin >> T;
+    while(T--)
+        solve();
+}
